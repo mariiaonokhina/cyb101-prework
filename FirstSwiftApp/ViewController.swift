d208fbbd12bc5c1feabb85e1cf1b0f9572abe76c
mariiaonokhina <83292730+mariiaonokhina@users.noreply.github.com>
2025-05-04 18:08:20 -0400

    Initial Commit

---
+++ FirstSwiftApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FirstSwiftApp
+//
+//  Created by Mariia Onokhina on 5/4/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
